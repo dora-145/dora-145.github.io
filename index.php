5b3429926e40b4a42d521a44ad8c3dd4186bbdeb
dora-145 <isadora.passos145@gmail.com>
2024-10-26 21:28:00 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Minha página de teste</title>
+  </head>
+  <body>
+    <h1>Teste</h1>
+    <?php>echo('Teste')<?php>
+  </body>
+</html>
